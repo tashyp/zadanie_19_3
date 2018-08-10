0fa880c13937219e1fff256decbc0063018ca5d9
Przemek Saja <przemek.saja@gmail.com>
2018-08-10 15:11:15 +0200

    Add file with actions

---
+++ actions.js
@@ -0,0 +1,48 @@
+//actions.js
+
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text: text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id: id,
+        text: text
+    }
+}
+
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id: id
+    }
+}
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        value: +1,
+        id: id
+    }
+}
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        value: -1,
+        id: id
+    }
+}
